77396807646e3809aac4b3d9ea37dfd47cce38ab
gavintobin <gavintobin714@gmail.com>
2022-12-15 12:31:49 -0800

    final push

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,7 +9,7 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *nn = malloc(sizeof(nn));
+	binary_tree_t *nn = malloc(sizeof(binary_tree_t));
 
 	if (nn == NULL)
 	return (NULL);
