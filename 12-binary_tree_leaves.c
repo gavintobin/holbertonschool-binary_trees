caf9e756b4eb9df40d99b2075556048ee82118f8
gavintobin <gavintobin714@gmail.com>
2022-12-19 05:52:56 -0800

    same thing

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,10 +8,11 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	int count = 0;
+
 	if (tree == NULL)
 	return (0);
 
-	int count = 0;
 
 	count += binary_tree_leaves(tree->left);
 	count += binary_tree_leaves(tree->right);
